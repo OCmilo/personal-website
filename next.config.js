15dbf56897a9321971e979918522dca5f9d75235
Camilo <camilocarneiro@hotmail.com>
2020-12-19 18:45:28 -0300

    adiciona redirecionamentos pro site dela

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const { getRedirectStatus } = require("next/dist/lib/load-custom-routes");
+
 module.exports = {
   i18n: {
     locales: ['en', 'pt'],
@@ -6,4 +8,18 @@ module.exports = {
   images: {
     deviceSizes: [312, 500, 800],
   },
+  async redirects() {
+    return [
+      {
+        source: '/girlfriend',
+        destination: 'https://catarinaramos.com.br',
+        permanent: true,
+      },
+      {
+        source: '/namorada',
+        destination: 'https://catarinaramos.com.br',
+        permanent: true,
+      },
+    ]
+  }
 }
